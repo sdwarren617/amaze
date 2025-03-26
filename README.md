021de470dd7f372c0194346f1f834b174d81137d
Barak Binaymin <barak.binyamin555@gmail.com>
2025-03-25 22:19:12 -0400

    Update docs

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A fun game to learn to code by moving a robot through a maze. Start by moving a robot, end by launching your own website
 
 Lesson plan
-1. Download this starter code and make a nice coding setup
+1. Download this starter code and get a good coding setup
 2. Play the game by coding the robot through the maze
 3. Change the website by updating text, images, and game logic 
 4. Publish your version of the game to the internet
@@ -21,17 +21,27 @@ What are all these files for?
 | [style.css](style.css)         | Style code to make the website pretty       |
 
 
-#### Lesson 1: Getting started
-1. Sign up for github
-2. "Fork this repo"
-3. Get vscode
-4. Add this cool extension to preview websites with live changes
+#### Part 1: Getting started
+1. Sign up for [github](https://github.com/signup)
+2. [Get vscode](https://code.visualstudio.com/download)
+3. "Fork this repo", download your copy, and open in vscode
+4. Add the [live preview](vscode:extension/ms-vscode.live-server) extension to preview changes you make in real time
+5. Two finger click on the README.md file and select open preview, feel free to play around with editing these docs as a good start to coding
 
-#### Lesson 2: Play the game
+#### Part 2: Play the game
+<img src="lesson-images/capture.gif"/>
 
-#### Lesson 3: Customize the game
+#### Part 3: Customize the game
+Explore the code, change stuff and see what happens
 
-#### Lesson 4: Publish your custom website
+#### Part 4: Publish your custom website
+1. Click on the circle branch sidebar option
+2. Type "My first commit" in the Messsage box
+3. Press Commit, Press Push
+4. Change the settings on your github repo to turn on web hosting mode
+5. Navigate to yourccount.github.io/amaze to see your hosted website
+
+Great work! You've published your own website!!! 
 
 ### So you wanna make your own website?
 Here's some inpsiration `🌶️ = exponential perspiration`
@@ -42,8 +52,8 @@ Here's some inpsiration `🌶️ = exponential perspiration`
 
 #### Fancy "I Know how to code" Resume
 - [https://phlippe.github.io/](https://phlippe.github.io/)  🌶️ 🌶️ 🌶️ 
-- [https://alfredfrancis.in/](https://alfredfrancis.in/about/) simple
-
+- [https://alfredfrancis.in/](https://alfredfrancis.in/about/)
+- [http://vanderplas.com/](http://vanderplas.com/)
 
 <!-- ### Tech Docs -->
 
--- main.js
+++ main.js
@@ -3,40 +3,55 @@ class Robot{
         this.item = document.getElementById("ROBOT")
         this.item.style.left = "7px"
         this.item.style.top  = "7px"
-        this.x = 0
-        this.y = 0
+        this.x        = 0
+        this.y        = 0
+        this.barriers = [
+            [0, 1, 0, 0 ,0,], 
+            [0, 1, 0, 1 ,0,], 
+            [0, 1, 0, 1 ,0,], 
+            [0, 0, 0, 1 ,0,], 
+        ]
     }
     async move_left(){
         let position = parseInt(this.item.style.left)
         this.item.style.left = `${position-52}px`
         await new Promise(res=>setTimeout(res,600))
         this.x-=1
-        this.check_success()
+        await this.check_success()
     }
     async move_right(){
         let position = parseInt(this.item.style.left)
         this.item.style.left = `${position+52}px`
         await new Promise(res=>setTimeout(res,600))
         this.x+=1
-        this.check_success()
+        await this.check_success()
     }
     async move_up(){
         let position = parseInt(this.item.style.top)
         this.item.style.top = `${position-52}px`
         await new Promise(res=>setTimeout(res,600))
         this.y-=1
-        this.check_success()
+        await this.check_success()
     }
     async move_down(){
         let position = parseInt(this.item.style.top)
         this.item.style.top = `${position+52}px`
         await new Promise(res=>setTimeout(res,600))
         this.y+=1
-        this.check_success()
+        await this.check_success()
     }
-    check_success(){
-        if (this.x == 4 && this.y==3 ){
+    async check_success(){
+        // Check if we hit a rock
+        if (this.barriers[this.y][this.x] == 1){
+            document.body.style.background="red"
+            await new Promise(res=>setTimeout(res,100))
+            alert("Ahhhh you hit a wall! Try moving the robot where there aren't a bunch of rocks")
+            document.location.reload()
+        }
+        // check if we got to the goal
+        else if (this.x == 4 && this.y==3 ){
             document.getElementById("g").style['background-image'] = 'url("glitter.gif")'
+            document.body.style['background-image'] = 'url("glitter.gif")'
         }
     }
 }
@@ -53,9 +68,7 @@ async function main(){
     //      3. await robot.move_right()
     //      4. await robot.move_left()
 
-    await robot.move_down()  
-    
-    
-    
+    await robot.move_down()
+
     /**** Code within here to start  ****/
 }
